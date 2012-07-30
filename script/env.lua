fe9644a6d4711e2f67ae6eb105181ad8bbad2b5e
b <bram.wall@gmail.com>
2012-07-30 13:03:41 +0300

    deleted env, is generated on installation

--- script/env.lua
+++
@@ -1,7 +0,0 @@
-BASEPATH = "/home/bram/alphaserv-v4"
-
-package.path = BASEPATH.."/script/?/init.lua;" .. package.path
-package.path = BASEPATH.."/script/package/?/init.lua;" .. package.path
-package.path = BASEPATH.."/script/package/?.lua;" .. package.path
-package.path = BASEPATH.."/script/?.lua;" .. package.path
-package.cpath = BASEPATH.."/lib/lib?.so;" .. package.cpath
